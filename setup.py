8f74565c735675b9e67a95060471ead743ea8bbd
Taylor Oshan <tayoshan@gmail.com>
2018-09-14 20:14:58 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os.path
 from distutils.command.build_py import build_py
 
 # Get __version__ from PACKAGE_NAME/__init__.py without importing the package
 # __version__ has to be defined in the first line
 with open('spglm/__init__.py', 'r') as f:
     exec(f.readline())
+
+def _get_requirements_from_files(group_files):
+    groups_reqlist = {}
+
+    for k,v in groups_files.items():
+        with open (v, 'r') as f:
+            pkg_list = f.read().splitlines()
+        groups_reqlist[k] = pkg_list
+
+    return groups_reqlist
+
+
+def setup_package():
+
+    _groups_files = {
+        'base': 'requirements.txt', #basic requirements
+        'tests': 'requirements_tests.txt', #requirements for tests
+        'docs': 'requirements_docs.txt' #requirements for building docs
+    }
+    reqs = _get_requirements_from_files(_groups_files)
+    install_reqs = reqs.pop('base')
+    extras_reqs = reqs
 
 setup(name='spglm', #name of package
     version=__version__,
@@ -29,8 +52,11 @@ setup(name='spglm', #name of package
         'Programming Language :: Python :: 3.6'
         ],
     license='3-Clause BSD',
-    packages=['spglm'],
-    install_requires=['scipy', 'numpy', 'libpysal'],
+    packages=find_packages(),
+    install_requires=install_reqs,
+    extras_require=extras_reqs,
     zip_safe=False,
     cmdclass = {'build.py':build_py})
 
+if __name__ == '__main__':
+	setup_package()
