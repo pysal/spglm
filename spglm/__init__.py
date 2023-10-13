4e6d56ba540ea7a4125cb5236b2cb9079f73aeca
James Gaboardi <jgaboardi@gmail.com>
2023-10-12 22:10:47 -0400

    adopt pyproject.tml & setuptools_scm

--- spglm/__init__.py
+++ spglm/__init__.py
@@ -1,6 +1,10 @@
-__version__ = "1.0.8"
+import contextlib
+from importlib.metadata import PackageNotFoundError, version
 
 from . import glm  # noqa F401
 from . import family  # noqa F401
 from . import utils  # noqa F401
 from . import iwls  # noqa F401
+
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = version("spglm")
